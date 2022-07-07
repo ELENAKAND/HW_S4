44d308945f55edf7988b8a8de40bf4fbcee7d157
ELENA <elenakand@icloud.com>
2022-07-07 14:21:56 +0300

    Сумма цифр числа

---
+++ DigitdsSum/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int GetNumber(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Введите корректное число");
+        }
+    }
+    return result;
+}
+int DigitsSum(int number)
+{
+    int sum = 0;
+    for (int i = 0; number > 0; i++)
+    {
+        sum += number%10;
+        number = number/10;
+    }                 
+    return sum;
+ 
+}
+int number = GetNumber("Введите число"); 
+Console.WriteLine("Сумма цифр числа " + number + " = " + DigitsSum(number));
+
+
+
+
+
+
+

60fd952ed3ad83e7951d5d0782e4729221df811f
ELENA <elenakand@icloud.com>
2022-07-07 15:22:32 +0300

    Заполнение и выведение массива на 8 элементов

---
+++ FillPrintArray/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+void FillArray(int[]array)
+{
+    Random rnd = new Random();
+    int length = array.Length;
+    int index = 0;
+    for (index = 0; index < length; index ++)
+    {
+        array [index] = rnd.Next(1,100);
+    }
+}
+void PrintArray(int[]array)
+{
+    int length = array.Length;
+    int index = 0;
+    for (index = 0; index < length; index ++)
+    {
+        Console.WriteLine(array [index]);
+    }
+}
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
